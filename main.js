ace19dc86e34013112defcb9346282f1ca94aaba
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-03-16 23:24:00 -0400

    Replace let for const

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-let filterInput = document.getElementById('filterInput');
+const filterInput = document.getElementById('filterInput');
 filterInput.addEventListener('keyup', filterNames);
 
 function filterNames() {
-    let filterValue = document.getElementById('filterInput').value.toUpperCase();
-    let names = document.getElementById('names');
-    let items = names.querySelectorAll('li.collection-item');
+    const filterValue = document.getElementById('filterInput').value.toUpperCase();
+    const names = document.getElementById('names');
+    const items = names.querySelectorAll('li.collection-item');
 
     for (let i = 0; i < items.length; i++) {
-        let item = items[i].getElementsByTagName('a')[0];
+        const item = items[i].getElementsByTagName('a')[0];
         if (item.innerHTML.toUpperCase().indexOf(filterValue) > -1)
             items[i].style.display = '';
         else 
