73898d254e99e17e4ad79976a0c4b13918495023
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-03-16 23:23:36 -0400

    Rename variables

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ filterInput.addEventListener('keyup', filterNames);
 
 function filterNames() {
     let filterValue = document.getElementById('filterInput').value.toUpperCase();
-    let ul = document.getElementById('names');
-    let li = ul.querySelectorAll('li.collection-item');
+    let names = document.getElementById('names');
+    let items = names.querySelectorAll('li.collection-item');
 
-    for (let i = 0; i < li.length; i++) {
-        let a = li[i].getElementsByTagName('a')[0];
-        if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1)
-            li[i].style.display = '';
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i].getElementsByTagName('a')[0];
+        if (item.innerHTML.toUpperCase().indexOf(filterValue) > -1)
+            items[i].style.display = '';
         else 
-            li[i].style.display = 'none';
+            items[i].style.display = 'none';
     }
 }
