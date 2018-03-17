a7b1f29e53a9bc8725ce574608480e4b48985188
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-03-17 00:46:25 -0400

    Refactor the filterNames function

--- main.js
+++ main.js
@@ -2,15 +2,26 @@ const filterInput = document.getElementById('filterInput');
 filterInput.addEventListener('keyup', filterNames);
 
 function filterNames() {
-    const filterValue = document.getElementById('filterInput').value.toUpperCase();
+    const query = document.getElementById('filterInput').value.toUpperCase();  
     const names = document.getElementById('names');
     const items = names.querySelectorAll('li.collection-item');
+    
+    items.forEach(item => {
+        if (contains(item, query))
+            show(item);
+        else
+            hide(item);
+    });
+}
 
-    for (let i = 0; i < items.length; i++) {
-        const item = items[i].getElementsByTagName('a')[0];
-        if (item.innerHTML.toUpperCase().indexOf(filterValue) > -1)
-            items[i].style.display = '';
-        else 
-            items[i].style.display = 'none';
-    }
+function contains(element, text) {
+    return element.innerHTML.toUpperCase().includes(text);
+}
+
+function show(element) {
+    element.style.display = '';
+}
+
+function hide(element) {
+    element.style.display = 'none';
 }
